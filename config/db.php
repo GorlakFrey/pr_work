3523421f3443dcbe4032c5a81459c6d04c05845a
ClassUser <ClassUser@spbrtk.local>
2019-12-16 11:00:43 +0300

    two commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=prwork',
+    'dsn' => 'mysql:host=localhost;dbname=gor',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'enter your secret key here',
+            'cookieValidationKey' => 'fjjtltkmfmde5476568',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
